9c2039a140a6123fb8a1d7b6d5b764481db990f1
SourceSensei <source.sensei1205@gmail.com>
2023-11-10 19:22:17 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,114 @@
+<script>
+  import Header from "./UI/Header.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+  import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
+
+  let title = "";
+  let subtitle = "";
+  let address = "";
+  let email = "";
+  let description = "";
+  let imageUrl = "";
+
+  let meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "In this meetup, we will have some experts that can teach you how to code",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar.jpg/1024px-Caffe_Nero_coffee_bar.jpg",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "l7Y1R@example.com",
+    },
+    {
+      id: "m2",
+      title: "Swim Together",
+      subtitle: "Let's go for some swimming",
+      description: "We will simply swim some rounds!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Aquarium_in_Boston%2C_Massachusetts%2C_1870.jpg/800px-Aquarium_in_Boston%2C_Massachusetts%2C_1870.jpg",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "l7Y1R@example.com",
+    },
+  ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      address: address,
+      contactEmail: email,
+    };
+
+    meetups = [newMeetup, ...meetups];
+  }
+</script>
+
+<Header />
+
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      type="text"
+      value={title}
+      on:input={(event) => (title = event.target.value)}
+    />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      type="text"
+      value={subtitle}
+      on:input={(event) => (subtitle = event.target.value)}
+    />
+    <TextInput
+      id="address"
+      label="Address"
+      type="text"
+      value={address}
+      on:input={(event) => (address = event.target.value)}
+    />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      type="text"
+      value={imageUrl}
+      on:input={(event) => (imageUrl = event.target.value)}
+    />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      value={email}
+      type="email"
+      on:input={(event) => (email = event.target.value)}
+    />
+    <TextInput
+      id="description"
+      label="Description"
+      controllType="textarea"
+      value={description}
+      on:input={(event) => (description = event.target.value)}
+    />
+    <Button type="submit" caption="Save" />
+  </form>
+  <MeetupGrid {meetups} />
+</main>
+
+<style>
+  main {
+    margin-top: 5rem;
+  }
+
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
+</style>
